fbb6e5e5dd6c0a330fc689af53022fb4c745c10a
cgcgbcbc <cgcgbcbc@163.com>
2014-01-26 22:48:16 +0800

    add implement and test for route '/query'

---
+++ infu/app-test.py
@@ -0,0 +1,81 @@
+__author__ = 'guangchen'
+
+import unittest
+import json as jsonlib
+from app import app
+
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['es_url'] = 'http://www.lab.mu:9200/'
+        self.app = app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_query_without_params(self):
+        response = self.app.get('/query')
+        data = response.data
+        result = jsonlib.loads(data)
+        assert result['count'] == 10
+        item = result['result'][0]
+        assert u'resultIndex' in item
+        assert u'type' in item
+
+    def test_query_with_keywords(self):
+        response = self.app.get('/query?keywords=a')
+        data = response.data
+        result = jsonlib.loads(data)
+        assert result['count'] == 10
+        item = result['result'][0]
+        assert u'a' in item['question'] or u'a' in item['answer'] or u'a' in item['title']
+
+    def test_query_with_size(self):
+        response = self.app.get('/query?size=15')
+        result = jsonlib.loads(response.data)
+        assert result['count'] == 15
+
+    def test_query_with_type(self):
+        response = self.app.get('/query?type=["tunet"]')
+        result = jsonlib.loads(response.data)
+        item = result['result'][0]
+        assert u'tunet' == item['type']
+
+    def test_query_with_start(self):
+        response = self.app.get('/query')
+        origin_result = jsonlib.loads(response.data)
+        response = self.app.get('/query?start=1')
+        offset_result = jsonlib.loads(response.data)
+        origin_item = origin_result['result'][1]
+        offset_item = offset_result['result'][0]
+        assert origin_item['type'] == offset_item['type']
+        if origin_item['type'] == u'infu':
+            assert origin_item['title'] == offset_item['title']
+        else:
+            assert offset_item['link'] == offset_item['link']
+
+    def test_query_by_post(self):
+        headers = [('Content-Type', 'application/json')]
+        result = jsonlib.loads(self.app.post('/query', {}).data)
+        assert result['count'] == 10
+        item = result['result'][0]
+        assert u'resultIndex' in item
+        assert u'type' in item
+        data = dict(keywords=u'a', size=15, start=0, type=[u'lib'])
+        # json_data = jsonlib.dumps(data)
+        # json_data_length = len(json_data)
+        # headers.append(('Content-Length', json_data_length))
+        data = self.app.post('/query', headers=headers, data=jsonlib.dumps(data)).data
+        origin_result = jsonlib.loads(data)
+        offset_result = jsonlib.loads(data)
+        assert origin_result['count'] == 15
+        origin_item = origin_result['result'][1]
+        offset_item = offset_result['result'][0]
+        assert offset_item['type'] == origin_item['type'] == u'lib'
+        assert offset_item['link'] == offset_item['link']
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ infu/app/esparse.py
@@ -0,0 +1,168 @@
+# coding=utf-8
+__author__ = 'guangchen'
+import re
+
+
+class EsParse:
+    def __init__(self, es_result=None):
+        if es_result is None:
+            es_result = []
+        result = []
+        index = 0
+        for item in es_result:
+            index += 1
+            parse_item = self.__parse(item)
+            parse_item['resultIndex'] = index
+            result.append(parse_item)
+        self.result = result
+
+    # noinspection PyProtectedMember
+    def __parse(self, item):
+        item_type = item._meta.type
+        if item_type == u'infu':
+            return InfuResult(item).result
+        elif item_type in [u'artschool', u'learnfaq', u'lib']:
+            return CommonResult(item).result
+        elif item_type == u'career':
+            return Career(item).result
+        elif item_type == u'chemschool':
+            return Chemschool(item).result
+        elif item_type == u'lib_author':
+            return LibAuthor(item).result
+        elif item_type == u'myhome':
+            return MyHome(item).result
+        elif item_type == u'tunet':
+            return Tunet(item).result
+        return {}
+
+
+class Tunet():
+    def __init__(self, es_item):
+        self.result = {}
+        self.result.update({
+            'type': u'tunet',
+            'question': es_item.question,
+            'answer': es_item.answer,
+            'link': es_item.link,
+            'date': es_item.date,
+        })
+
+
+class MyHome():
+    def __init__(self, es_item):
+        self.result = {}
+        self.result.update({
+            'type': u'myhome',
+            'question': es_item.question,
+            'answer': es_item.answer,
+            'link': es_item.link,
+            'title': es_item.title,
+        })
+
+
+class LibAuthor():
+    def __init__(self, es_item):
+        self.result = {}
+        self.result.update({
+            'type': u'libauthor',
+            'question': es_item.question,
+            'date': es_item.date,
+            'answer': es_item.answer,
+            'link': es_item.link,
+            'title': es_item.title,
+            'reDate': es_item.reDate,
+        })
+
+
+class CommonResult():
+    # noinspection PyProtectedMember
+    def __init__(self, es_item):
+        self.result = {}
+        self.result.update({
+            'type': es_item._meta.type,
+            'question': es_item.question,
+            'answer': es_item.answer,
+            'link': es_item.link,
+        })
+
+
+class Chemschool():
+    def __init__(self, es_item):
+        self.result = {}
+        self.result.update({
+            'type': u'chemschool',
+            'question': es_item.question,
+            'date': es_item.date,
+            'answer': es_item.answer,
+            'link': es_item.link,
+            'title': es_item.title,
+            'exactDate': es_item.exactDate,
+        })
+
+
+class Career():
+    def __init__(self, es_item):
+        self.result = {}
+        self.result.update({
+            'type': u'career',
+            'question': es_item.question,
+            'answer': es_item.answer,
+            'link': es_item.link,
+            'title': es_item.title,
+        })
+
+
+class InfuResult():
+    # noinspection PyProtectedMember
+    def __init__(self, r):
+        self.result = {}
+        comment_list = []
+        for com in r.comm.comments:
+            comment_list.append(
+                {
+                    'body': com.body,
+                    'name': com.name,
+                    'time': com.time
+                }
+            )
+        title = r.info.title
+        if 'highlight' in r._meta:
+            title = r._meta['highlight']['info.title'][0]
+        author_and_id = self.get_author_and_id(title)
+        author = ""
+        author_id = ""
+        if author_and_id != "":
+            title = self.get_title(title)
+            author = self.get_author(author_and_id)
+            author_id = self.get_author_id(author_and_id)
+        self.result.update({
+            # 'meta': r._meta,
+            'title': title,
+            'author': author,
+            'authorId': author_id,
+            'largeUrl': r.info.largeUrl,
+            'time': r.info.time,
+            'commentCount': r.comm.commentCount,
+            'commentList': comment_list,
+            'photoId': r.info.photoId,
+            'type': u'infu'
+        })
+
+    def get_title(self, title):
+        return self.get_re_match(title, ur'@.*?问:(.*$)', re.DOTALL)
+
+    def get_re_match(self, source, reg, flag=0):
+        r = re.compile(reg, flag)
+        obj = r.match(source)
+        if obj is not None:
+            return obj.group(1)
+        return ""
+
+    def get_author_and_id(self, title):
+        return self.get_re_match(title, ur'@(.*?)问:')
+
+    def get_author_id(self, author_and_id):
+        return self.get_re_match(author_and_id, ur'.*\(([0-9]*?)\s*\)\s*$')
+
+    def get_author(self, author_and_id):
+        return self.get_re_match(author_and_id, ur'(.*)\([0-9]*?\s*\)\s*$')
