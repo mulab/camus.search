47237d6eafcb1545ca4ad96040832b8bbe302570
cgcgbcbc <cgcgbcbc@163.com>
2014-01-27 23:34:06 +0800

    construct the project
    Using requirejs, jquery, underscore, backbone.
    use nproxy to isolate frontend project and backend
    project for developing

--- infu/frontend/proxy.js
+++ infu/frontend/proxy.js
@@ -5,7 +5,27 @@ var fs = require('fs');
 var path = fs.realpathSync('.');
 module.exports = [
     {
-        pattern: 'localhost:8000/static/require.js',
+        pattern: 'static/js/lib/require.js',
         responder: path+'/bower_components/requirejs/require.js'
+    },
+    {
+        pattern: 'static/js/app.js',
+        responder: path+'/js/app.js'
+    },
+    {
+        pattern: 'static/js/lib/jquery.js',
+        responder: path+'/bower_components/jquery/jquery.js'
+    },
+    {
+        pattern: 'static/js/lib/underscore.js',
+        responder: path+'/bower_components/underscore/underscore.js'
+    },
+    {
+        pattern: 'static/js/lib/backbone.js',
+        responder: path+'/bower_components/backbone/backbone.js'
+    },
+    {
+        pattern: 'static/js/lib/handlebars.js',
+        responder: path+'/bower_components/handlebars/handlebars.js'
     }
 ];
