d6265968815d499b1fb9b396ae21722bc12a242b
cgcgbcbc <cgcgbcbc@163.com>
2014-01-28 23:11:55 +0800

    add my home model and tests

--- infu/frontend/js/test/Fixture.js
+++ infu/frontend/js/test/Fixture.js
@@ -11,5 +11,18 @@ define(function () {
         "resultIndex": 1,
         "type": "tunet"
     };
-    return {Tunet:tunet};
+    var my_home = {
+        "answer": [
+            "\r\n            \r\n                 \r\n            \r\n        \r\n    \r\n\r\n    \u6682\u65f6\u6ca1\u6709\u7b54\u590d\u4fe1\u606f\uff01\r\n\r\n\r\n    \r\n        \r\n    \r\n\r\n\r\n        \r\n\r\n    \r\n    \r\n\r\n\r\n    \u6682\u65f6\u6ca1\u6709\u540c\u5b66\u8ba8\u8bba\u4fe1\u606f\uff01\r\n\r\n\r\n    "
+        ],
+        "link": "http://myhome.tsinghua.edu.cn/Netweb_List/Netweb_Home_Advice_Detail_8505f414a521e0a12110.html",
+        "question": "\u5783\u573e\u8f66\u6bcf\u6b21\u6536\u5b8c\u5783\u573e\uff0c\u5730\u9762\u4e0a\u90fd\u7559\u4e0b\u810f\u810f\u7684\u5370\u5b50\uff0c\u80fd\u4e0d\u80fd\u6539\u8fdb\u4e00\u4e0b\u5462\uff1f\u65e2\u4e0d\u536b\u751f\u53c8\u4e0d\u7f8e\u89c2\u7684\u3002\u7d2b\u8346W\u697c\u8fd9\u8fb9\u7684\u597d\u591a\u8def\u9762\u90fd\u5f88\u810f\uff0c\u8fd8\u6709\u98df\u5802\u540e\u9762\u7684\u4e5f\u5b58\u5728\u540c\u6837\u95ee\u9898\u3002",
+        "resultIndex": 1,
+        "title": "\u5b66\u6821\u5783\u573e\u8f66\u5de5\u4f5c\u6280\u672f\u9700\u8981\u6539\u8fdb",
+        "type": "myhome"
+    };
+    return {
+        Tunet:tunet,
+        MyHome:my_home
+    };
 });
