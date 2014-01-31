d0287f8b9d291486df75d8c78551565dbf4f93f2
cgcgbcbc <cgcgbcbc@163.com>
2014-01-31 22:40:35 +0800

    add result list model and tests

--- infu/frontend/js/test/Fixture.js
+++ infu/frontend/js/test/Fixture.js
@@ -71,6 +71,253 @@ define(function () {
         "title": " \u660e\u5929\u8001\u6bd5\u7684\u5927\u96fe\u82f1\u8981\u8003\u8bd5\u4e86\uff0c\u600e\u4e48\u590d\u4e60\u554a\uff1f\u6709\u4ec0\u4e48\u9898\u53ef\u505a\u4e48\uff1f",
         "type": "infu"
     };
+    var result_list = {
+        "count": 10,
+        "result": [
+            {
+                "answer": "It depends. Usually you should download in a speed which is comparable to a reasonable reading speed. Otherwise, databases have the right to block all users from Tsinghua University.",
+                "link": "http://vrs.lib.tsinghua.edu.cn/pub/FAQ_View.asp?id=1249&ClassId=78&SearchStr=",
+                "question": "How many papers I can download in a database?",
+                "resultIndex": 0,
+                "type": "lib"
+            },
+            {
+                "answer": "You have to select Unicode (UTF-8) to get a chat record via email.",
+                "link": "http://vrs.lib.tsinghua.edu.cn/pub/FAQ_View.asp?id=1225&ClassId=79&SearchStr=",
+                "question": "Why I cannot receive a chat record?",
+                "resultIndex": 1,
+                "type": "lib"
+            },
+            {
+                "author": "\u5362\u81fb",
+                "authorId": "337840924",
+                "commentCount": 25,
+                "commentList": [
+                    {
+                        "body": "\u6211\u64e6... ...\u6de1\u5b9a... ...",
+                        "name": "\u95eb\u6770",
+                        "time": "2013-02-24 22:09"
+                    },
+                    {
+                        "body": "\u56de\u590d\u5434\u56fd\u6881\uff1a\u6478\u6478~\u86cb\u5b9a..",
+                        "name": "\u5362\u81fb",
+                        "time": "2013-02-24 21:52"
+                    },
+                    {
+                        "body": "\u6211\u90fd\u6eda\u6765\u534f\u548c\u4e86\u3002\u3002\u3002<xiaonei_wap/>",
+                        "name": "\u5434\u56fd\u6881",
+                        "time": "2013-02-24 21:52"
+                    },
+                    {
+                        "body": "\u5df2\u6709\u7740\u843d\u3002\u3002\u591a\u8c22\u5927\u5bb6~",
+                        "name": "\u5362\u81fb",
+                        "time": "2013-02-24 21:11"
+                    },
+                    {
+                        "body": "\u597d\u60b2\u5267\u989d\u3002\u3002\u540c\u60c5\u4e00\u4e2a\u3002\u3002\u3002",
+                        "name": "\u5415\u666f\u701a\u2122GT",
+                        "time": "2013-02-24 20:47"
+                    },
+                    {
+                        "body": "\u56de\u590d\u8463\u60a0\u76c8: \u5bf9\u554a\uff0c\u518d\u4e0d\u8bf7\u5973\u751f\u8282\u90fd\u8981\u6765\u4e86<xiaonei_wap/>",
+                        "name": "\u9ad8\u6e0a",
+                        "time": "2013-02-24 20:23"
+                    },
+                    {
+                        "body": "\u56de\u590d\u5362\u81fb\uff1a\u5feb\u70b9\u8bf7\u6389\uff0c\u4e0d\u8981\u62d6\u4e86~",
+                        "name": "\u8463\u60a0\u76c8",
+                        "time": "2013-02-24 20:21"
+                    },
+                    {
+                        "body": "\u56de\u590d\u8463\u60a0\u76c8\uff1a\u4f60\u7684\u5fc3\u613f\u8fbe\u6210\u4e86= =",
+                        "name": "\u5362\u81fb",
+                        "time": "2013-02-24 20:20"
+                    },
+                    {
+                        "body": "\u56de\u590d\u9ad8\u6e0a\uff1aYeah\uff0c\u9ad8\u6e0a\u4e07\u5c81\uff01\u3001",
+                        "name": "\u8463\u60a0\u76c8",
+                        "time": "2013-02-24 20:20"
+                    },
+                    {
+                        "body": "\u56de\u590d\u8463\u60a0\u76c8: \u6211\u5df2\u7ecf\u7ed9\u4ed6\u627e\u5230\u4eba\u4e86\uff0c\u4ed6\u5fc5\u987b\u8981\u8bf7\u5ba2\u4e86<xiaonei_wap/>",
+                        "name": "\u9ad8\u6e0a",
+                        "time": "2013-02-24 20:19"
+                    },
+                    {
+                        "body": "\u56de\u590d\u5362\u81fb\uff1a<img src='http://a.xnimg.cn/imgpro/emotions/tie/15.gif' alt='\u53f9\u6c14'  />",
+                        "name": "\u8463\u60a0\u76c8",
+                        "time": "2013-02-24 20:17"
+                    },
+                    {
+                        "body": "\u56de\u590d\u8463\u60a0\u76c8\uff1a\u9976\u547d..",
+                        "name": "\u5362\u81fb",
+                        "time": "2013-02-24 20:15"
+                    },
+                    {
+                        "body": "\u56de\u590d\u5362\u81fb\uff1a\u5148\u7b54\u5e94\u79ef\u70b9RP\u5427\u3002\u3002\u3002",
+                        "name": "\u8463\u60a0\u76c8",
+                        "time": "2013-02-24 20:15"
+                    },
+                    {
+                        "body": "\u56de\u590d\u8463\u60a0\u76c8\uff1a<img src='http://a.xnimg.cn/imgpro/icons/statusface/shangbuqi.gif' alt='\u4f24\u4e0d\u8d77'  />\u7b49\u6211\u6362\u5230\u4e86\u8bfe..",
+                        "name": "\u5362\u81fb",
+                        "time": "2013-02-24 20:14"
+                    },
+                    {
+                        "body": "<a href='http://www.renren.com/g/337773238' namecard='337773238' target='_blank'>@\u63e3\u5b66\u78ca</a> <a href='http://www.renren.com/g/324772055' namecard='324772055' target='_blank'>@\u5434\u4e00\u837b</a> <a href='http://www.renren.com/g/250468959' namecard='250468959' target='_blank'>@\u95eb\u6770</a> <a href='http://www.renren.com/g/265276179' namecard='265276179' target='_blank'>@\u5415\u666f\u701a\u2122GT</a> <a href='http://www.renren.com/g/281628366' namecard='281628366' target='_blank'>@\u6b66\u4e4b\u660a</a> <a href='http://www.renren.com/g/331557848' namecard='331557848' target='_blank'>@\u5434\u56fd\u6881</a> <a href='http://www.renren.com/g/240928274' namecard='240928274' target='_blank'>@\u5f20\u857e</a> \u591a\u8c22\u4e86\uff01~",
+                        "name": "\u5362\u81fb",
+                        "time": "2013-02-24 20:13"
+                    },
+                    {
+                        "body": "\u770b\u5728\u4f60\u5973\u670b\u53cb\u7684\u9762\u5b50\u4e0a\u8f6c\u4e86\uff0c\u8fd8\u4e0d\u8bf7\u6211\u4eec\u5927\u5bb6\u5403\u996d...",
+                        "name": "\u8463\u60a0\u76c8",
+                        "time": "2013-02-24 20:13"
+                    },
+                    {
+                        "body": "<a href='http://www.renren.com/g/249682377' namecard='249682377' target='_blank'>@\u674e\u667a</a> <a href='http://www.renren.com/g/314151963' namecard='314151963' target='_blank'>@\u6731\u851a\u6021</a> <a href='http://www.renren.com/g/249994089' namecard='249994089' target='_blank'>@\u80e1\u51cc\u7433</a> <a href='http://www.renren.com/g/262591368' namecard='262591368' target='_blank'>@\u8463\u60a0\u76c8</a> <a href='http://www.renren.com/g/340448717' namecard='340448717' target='_blank'>@\u9ad8\u6e0a</a> <a href='http://www.renren.com/g/327460571' namecard='327460571' target='_blank'>@\u674e\u5146\u9f91</a> <a href='http://www.renren.com/g/331721291' namecard='331721291' target='_blank'>@\u9648\u6960</a> <a href='http://www.renren.com/g/347024688' namecard='347024688' target='_blank'>@\u4f59\u70af</a> <a href='http://www.renren.com/g/332074573' namecard='332074573' target='_blank'>@\u5b5f\u6d69</a> \u9ebb\u70e6\u5927\u5bb6\u4e86\uff01\uff01\uff01<img src='http://a.xnimg.cn/imgpro/emotions/tie/10.gif?ver=1' alt='\u9177'  />",
+                        "name": "\u5362\u81fb",
+                        "time": "2013-02-24 20:10"
+                    },
+                    {
+                        "body": "\u56de\u590d\u725f\u77b3\uff1a\u90a3\u5c31\u6c42\u5206\u4eab\u5427\uff01",
+                        "name": "\u5362\u81fb",
+                        "time": "2013-02-24 20:07"
+                    },
+                    {
+                        "body": "<a href='http://www.renren.com/g/307574215' namecard='307574215' target='_blank'>@\u65b9\u9547\u6f8e</a> <a href='http://www.renren.com/g/334692397' namecard='334692397' target='_blank'>@\u6768\u8d85\u51e1</a> <a href='http://www.renren.com/g/294694917' namecard='294694917' target='_blank'>@\u8d75\u946b</a> <a href='http://www.renren.com/g/330883618' namecard='330883618' target='_blank'>@\u8096\u6c49</a> <a href='http://www.renren.com/g/333198489' namecard='333198489' target='_blank'>@\u94b1\u5803</a> <a href='http://www.renren.com/g/331202345' namecard='331202345' target='_blank'>@\u6c5f\u6c84</a> <a href='http://www.renren.com/g/343299791' namecard='343299791' target='_blank'>@\u5b59\u4fee\u6587</a> \u591a\u8c22\u4e86\u3002\u3002",
+                        "name": "\u5362\u81fb",
+                        "time": "2013-02-24 20:07"
+                    },
+                    {
+                        "body": "\u6211\u662f5-3\u7684\u771f\u662f\u62b1\u6b49\u4e86~",
+                        "name": "\u725f\u77b3",
+                        "time": "2013-02-24 20:07"
+                    },
+                    {
+                        "body": "<a href='http://www.renren.com/g/326902827' namecard='326902827' target='_blank'>@\u5218\u6653\u5b87</a> <a href='http://www.renren.com/g/248244208' namecard='248244208' target='_blank'>@\u51af\u4e50\u6850</a> <a href='http://www.renren.com/g/339741199' namecard='339741199' target='_blank'>@\u5ed6\u5a01</a> <a href='http://www.renren.com/g/328934793' namecard='328934793' target='_blank'>@\u66f9\u8d8a</a> <a href='http://www.renren.com/g/339545830' namecard='339545830' target='_blank'>@\u53f6\u7eff\u7d20</a> <a href='http://www.renren.com/g/271816999' namecard='271816999' target='_blank'>@\u91d1\u5b87\u8d85</a> <a href='http://www.renren.com/g/262238171' namecard='262238171' target='_blank'>@\u5f90\u535a\u9633BHSFer</a> <a href='http://www.renren.com/g/264379220' namecard='264379220' target='_blank'>@\u53f6\u8000\u8f89</a> <a href='http://www.renren.com/g/342682014' namecard='342682014' target='_blank'>@\u7a0b\u6797\u677e</a> ",
+                        "name": "\u5362\u81fb",
+                        "time": "2013-02-24 20:05"
+                    },
+                    {
+                        "body": "<a href='http://www.renren.com/g/271008176' namecard='271008176' target='_blank'>@\u5434\u6cfd\u5b99GRE+TOEFL</a> <a href='http://www.renren.com/g/338520217' namecard='338520217' target='_blank'>@\u4e8e\u6052</a> <a href='http://www.renren.com/g/338397234' namecard='338397234' target='_blank'>@\u6234\u5175.mol</a> <a href='http://www.renren.com/g/338335788' namecard='338335788' target='_blank'>@\u9ec4\u6d69\u672a</a> <a href='http://www.renren.com/g/327585977' namecard='327585977' target='_blank'>@\u738b\u79ba\u6dc7</a> <a href='http://www.renren.com/g/335791879' namecard='335791879' target='_blank'>@\u53f8\u5f92\u9759\u5f18</a> <a href='http://www.renren.com/g/343284220' namecard='343284220' target='_blank'>@\u80e1\u6653</a> <a href='http://www.renren.com/g/248933312' namecard='248933312' target='_blank'>@\u8096\u535a.lc</a> <a href='http://www.renren.com/g/343228201' namecard='343228201' target='_blank'>@\u6234\u60a6</a> <a href='http://www.renren.com/g/338982832' namecard='338982832' target='_blank'>@\u674e\u5f66\u9752</a> \u591a\u8c22\u5927\u5bb6= =",
+                        "name": "\u5362\u81fb",
+                        "time": "2013-02-24 20:03"
+                    },
+                    {
+                        "body": "<a href='http://www.renren.com/g/342705664' namecard='342705664' target='_blank'>@\u8096\u6743\u592b</a> <a href='http://www.renren.com/g/327779678' namecard='327779678' target='_blank'>@\u5f20\u5fd7\u4e1c</a> <a href='http://www.renren.com/g/331293039' namecard='331293039' target='_blank'>@\u5f20\u6cf0\u6e90</a> <a href='http://www.renren.com/g/313620754' namecard='313620754' target='_blank'>@\u8096\u6c38\u535a</a> <a href='http://www.renren.com/g/337253320' namecard='337253320' target='_blank'>@\u6e29\u4f7a</a> <a href='http://www.renren.com/g/331284844' namecard='331284844' target='_blank'>@\u5218\u71a0\u65fb</a> <a href='http://www.renren.com/g/333732044' namecard='333732044' target='_blank'>@\u5e94\u7fd4.D</a> <a href='http://www.renren.com/g/337839074' namecard='337839074' target='_blank'>@\u970d\u5fc3\u613f\u273floli</a> <a href='http://www.renren.com/g/328447994' namecard='328447994' target='_blank'>@\u66f2\u6d69\u7136 Rikimaru</a> <a href='http://www.renren.com/g/338986027' namecard='338986027' target='_blank'>@\u5b59\u601d\u8fdc.\u0389\u03bb\u03b9\u03bf\u03c2</a> \u5927\u5bb6\u61c2\u7684~~",
+                        "name": "\u5362\u81fb",
+                        "time": "2013-02-24 20:02"
+                    },
+                    {
+                        "body": "<a href='http://www.renren.com/g/333904232' namecard='333904232' target='_blank'>@\u9648\u6d2a\u949f</a> <a href='http://www.renren.com/g/338419194' namecard='338419194' target='_blank'>@\u738b\u658c\u65ed</a> <a href='http://www.renren.com/g/339154300' namecard='339154300' target='_blank'>@\u6731\u65bd\u5c55</a> <a href='http://www.renren.com/g/332943063' namecard='332943063' target='_blank'>@\u9648\u51ef</a> <a href='http://www.renren.com/g/339551341' namecard='339551341' target='_blank'>@\u90dd\u5411\u950b</a> <a href='http://www.renren.com/g/322750843' namecard='322750843' target='_blank'>@\u6731\u6657</a> <a href='http://www.renren.com/g/275193980' namecard='275193980' target='_blank'>@\u5218\u6148\u822a</a> <a href='http://www.renren.com/g/343159588' namecard='343159588' target='_blank'>@\u8096\u8d3a</a> <a href='http://www.renren.com/g/331814487' namecard='331814487' target='_blank'>@\u4e8e\u5e86\u6db5.php</a> \u5927\u5bb6\u61c2\u7684~\u6c42\u5206\u4eab\u554a<img src='http://a.xnimg.cn/imgpro/icons/statusface/cry.gif' alt='\u54ed'  />",
+                        "name": "\u5362\u81fb",
+                        "time": "2013-02-24 20:00"
+                    },
+                    {
+                        "body": "\u6269\u6563\uff01\uff01\uff01\u6709\u6761\u4ef6\u7684\u884c\u5584\u79ef\u5fb7\u6512\u4eba\u54c1\u554a\uff1e\uff1c\uff1e\uff1c\u301c\u301c",
+                        "name": "\u5468\u6851",
+                        "time": "2013-02-24 19:48"
+                    }
+                ],
+                "largeUrl": "http://fmn.rrfmn.com/fmn058/20130224/1935/p_large_vFX0_0b6c000011e41263.jpg",
+                "photoId": "6973140426",
+                "resultIndex": 2,
+                "time": "2013-02-24 19:35:57.0",
+                "title": " \u3010\u6362\u8bfe\u3011\u8dea\u6c42\u9009\u52305-2\u3010\u53f0\u7403\u8bfe\u3011\u7684\u597d\u5fc3\u4eba\u8ddf\u6211\u63625-3\u7684\uff01\uff01\u9662\u7cfb\u8bfe\u7a0b\u8c03\u6574\u5bfc\u81f4\u65f6\u95f4\u51b2\u7a81\u4f24\u4e0d\u8d77\u554a\u6709\u6728\u6709\uff01\uff01\u8dea\u6c42\u5404\u8def\u5927\u4fa0\u8f6c\u53d1\u5206\u4eabTT",
+                "type": "infu"
+            },
+            {
+                "author": "\u6768\u7693\u5468.Zircon.",
+                "authorId": "447001875",
+                "commentCount": 0,
+                "commentList": [],
+                "largeUrl": "http://fmn.rrimg.com/fmn064/20130530/1400/p_large_9W9R_5218000004e01263.jpg",
+                "photoId": "7191691933",
+                "resultIndex": 3,
+                "time": "2013-05-30 14:00:35.0",
+                "title": " \u7269\u7406\u5b9e\u9a8c<em>A</em>\u548cB\u6709\u4ec0\u4e48\u533a\u522b\u5462\uff1f\u9009B\u53ef\u4ee5\u66ff\u4ee3<em>A</em>\u5417\uff1f\u6c42\u95ee\uff01",
+                "type": "infu"
+            },
+            {
+                "author": "\u675c\u79b9\u8d24",
+                "authorId": "299554546",
+                "commentCount": 0,
+                "commentList": [],
+                "largeUrl": "http://fmn.rrfmn.com/fmn059/20130417/1535/p_large_uwHI_0715000023bf1262.jpg",
+                "photoId": "7084481001",
+                "resultIndex": 4,
+                "time": "2013-04-17 15:39:10.0",
+                "title": " \u6709\u6ca1\u6709\u4eca\u5e74\u79cb\u5b63\u53bbTexas <em>A</em>&amp;M\u7559\u5b66\u7684\u540c\u5b66\uff0c\u6c42\u7ec4\u961f\u554a~",
+                "type": "infu"
+            },
+            {
+                "author": "\u6bb7\u601d\u7476",
+                "authorId": "409428244",
+                "commentCount": 0,
+                "commentList": [],
+                "largeUrl": "http://fmn.rrimg.com/fmn065/20120906/2205/p_large_NR8k_0ecf0000b07a1263.jpg",
+                "photoId": "6507078148",
+                "resultIndex": 5,
+                "time": "2012-09-06 22:06:56.0",
+                "title": " \u6c4225,55\u7684\u5927\u5b66\u7269\u7406<em>A</em>(1)\u7684\u4e0a\u8bfe\u5730\u70b9~~ \u611f\u8c22!!",
+                "type": "infu"
+            },
+            {
+                "author": "\u8d56\u7fd4",
+                "authorId": "284935407",
+                "commentCount": 0,
+                "commentList": [],
+                "largeUrl": "http://fmn.rrimg.com/fmn065/20120607/1315/p_large_DOH8_31df000029711261.jpg",
+                "photoId": "6119070622",
+                "resultIndex": 6,
+                "time": "2012-06-07 13:16:56.0",
+                "title": " \u7269\u7406\u5b9e\u9a8c<em>A</em>(1)\u600e\u4e48\u9009\uff1f\u8981\u9009\u591a\u5c11\u65f6\u95f4\uff1f",
+                "type": "infu"
+            },
+            {
+                "author": "\u80e1\u5e7f\u53cb",
+                "authorId": "338683482",
+                "commentCount": 0,
+                "commentList": [],
+                "largeUrl": "http://fmn.rrfmn.com/fmn059/20130512/2210/p_large_CwbZ_3afb000007061263.jpg",
+                "photoId": "7149163790",
+                "resultIndex": 7,
+                "time": "2013-05-12 22:10:19.0",
+                "title": " \u9001\u4e00\u4e2a\u6709\u7ebf\u5feb\u95e8\u7ebf,\u6709\u610f\u8005\u6536\u4e4b,\u9002\u7528\u4e8e sony <em>a</em>\u7cfb\u5217,\u4ee5\u53ca Konica Minolta <em>a</em>\u7cfb\u5217\u548c7D/5D",
+                "type": "infu"
+            },
+            {
+                "author": "\u9ec4\u5ef6",
+                "authorId": "349380536",
+                "commentCount": 1,
+                "commentList": [
+                    {
+                        "body": "<a href='http://www.renren.com/g/340242786' namecard='340242786' target='_blank'>@\u9648\u6e90\u6cc9</a> ",
+                        "name": "\u674e\u4e00\u5e06",
+                        "time": "2013-02-23 19:28"
+                    }
+                ],
+                "largeUrl": "http://fmn.rrimg.com/fmn065/20130223/1815/p_large_y9b6_0743000004321262.jpg",
+                "photoId": "6970746933",
+                "resultIndex": 8,
+                "time": "2013-02-23 18:15:22.0",
+                "title": " ipv6\u4e0a\u4e0d\u53bb\uff0c\u600e\u4e48\u7834\uff1f",
+                "type": "infu"
+            },
+            {
+                "author": "\u5f20\u6587\u821f",
+                "authorId": "246582549",
+                "commentCount": 0,
+                "commentList": [],
+                "largeUrl": "http://fmn.rrimg.com/fmn060/20120909/1700/p_large_uxSE_5e2a000099691261.jpg",
+                "photoId": "6515920029",
+                "resultIndex": 9,
+                "time": "2012-09-09 17:03:44.0",
+                "title": " \u6c42\u6bb7\u4e1c\u751f\u8001\u5e08\u7684\u6570\u503c\u5206\u6790<em>A</em>\u7684\u4e0a\u8bfe\u5730\u70b9\uff0c\u591a\u8c22\uff01",
+                "type": "infu"
+            }
+        ]
+    };
     return {
         Tunet:tunet,
         MyHome:my_home,
@@ -78,6 +325,7 @@ define(function () {
         Chemschool:chemschool,
         Career:career,
         ArtSchool:art_school,
-        Infu:infu
+        Infu:infu,
+        ResultList:result_list
     };
 });
