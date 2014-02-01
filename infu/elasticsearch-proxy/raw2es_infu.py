52a2e4bfe25f502ef3cae3616bce72b2c02e331e
Hubertzhang <hubert.zyk@gmail.com>
2014-02-01 13:05:40 +0800

    crawler fixed

--- infu/elasticsearch-proxy/raw2es_infu.py
+++ infu/elasticsearch-proxy/raw2es_infu.py
@@ -14,7 +14,7 @@ doc_type = 'infu'  # document type is infu
 conn.indices.create_index_if_missing(index)
 
 mapping = json.load(open('mapping_infu.json'), encoding='utf-8')
-conn.put_mapping(doc_type=doc_type, mapping=mapping, indices=[index])
+conn.indices.put_mapping(doc_type=doc_type, mapping=mapping, indices=[index])
 
 path = u'../crawler/data/'
 reDIR = re.compile(ur'.*/\d{4}-\d{2} 提问 \((\d+)\).*')  # e.g. "data/2012-01 提问 (563247699)/"
@@ -35,7 +35,7 @@ for (dirpath, dirnames, filenames) in os.walk(path):
                 if not conn.exists(index=index, doc_type=doc_type, id=dirID + '_' + pid):
                     conn.index(whole, index=index, doc_type=doc_type, id=dirID + '_' + pid)
                 else:
-                    conn.update(whole, index=index, doc_type=doc_type, id=dirID + '_' + pid)
+                    conn.update(document=whole, index=index, doc_type=doc_type, id=dirID + '_' + pid)
 
-conn.optimize(indices=['qa'])
-conn.refresh()
+conn.indices.optimize(indices=['qa'])
+conn.indices.refresh()
