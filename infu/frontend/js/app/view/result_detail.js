c1d53cb124070ebd23f9cf1fcc50eaf62ccad7e6
cgcgbcbc <cgcgbcbc@163.com>
2014-02-01 22:03:46 +0800

    add result detail view and test

---
+++ infu/frontend/js/app/view/result_detail.js
@@ -0,0 +1,24 @@
+/**
+ * Created by guangchen on 2/1/14.
+ */
+"use strict";
+define(['jquery','underscore','backbone','handlebars','text!app/template/result_detail.html'],
+    function ($,_,Backbone,Handlebars,tmpl) {
+        Handlebars.registerHelper('iftype',function(doctype,options){
+            doctype = doctype.split(',');
+            if(_.indexOf(doctype,this.type)!=-1)
+                return options.fn(this);
+        });
+        return Backbone.View.extend({
+            template:tmpl,
+            render:function(){
+                var view = this, rendered,
+                    template = Handlebars.compile(view.template),
+                    model = this.model.toJSON();
+                rendered = template(model);
+                $(view.el).html(rendered);
+                return view;
+            }
+        });
+    }
+);
--- infu/frontend/js/test/views.js
+++ infu/frontend/js/test/views.js
@@ -2,31 +2,88 @@
  * Created by guangchen on 1/31/14.
  */
 "use strict";
-define(['app/view/result_list','app/model/result_list','app/model/query','test/Fixture','jquery'],function (View,Model,QueryModel,Fixture,$) {
-    var run = function(){
-        test('result list view',function(){
-            var model = new Model();
-            model.adapt_from(Fixture.ResultList);
-            model.set('query',new QueryModel({
-                keywords:'a',
-                type:7,
-                start:11
-            }));
-            var view = new View({
-                el:'#qunit-fixture',
-                model:model
+define(['app/view/result_list','app/view/result_detail','app/model/result_list','app/model/query','app/model_factory','test/Fixture','jquery'],
+        function (ResultListView,ResultDetailView,ResultListModel,QueryModel,factory,Fixture,$) {
+        var run = function(){
+            test('result list view',function(){
+                var model = new ResultListModel();
+                model.adapt_from(Fixture.ResultList);
+                model.set('query',new QueryModel({
+                    keywords:'a',
+                    type:7,
+                    start:11
+                }));
+                var view = new ResultListView({
+                    el:'#qunit-fixture',
+                    model:model
+                });
+                view.render();
+                var $fixture = $('#qunit-fixture');
+                console.log($fixture.html());
+                notEqual($fixture.html(),'','view is rendered');
+                equal($fixture.find('.result-item').length,10,'result item is correct');
+                var $pagination = $fixture.find('.result-pagination');
+                notEqual($pagination.html(),'','result pagination is rendered');
+                notEqual($pagination.html().indexOf('上一页'),-1,'pagination has pre page');
+                notEqual($pagination.html().indexOf('下一页'),-1,'pagination has next page');
+                equal($pagination.find('a').length,12,'pagination rendered correctly');
             });
-            view.render();
-            var $fixture = $('#qunit-fixture');
-            console.log($fixture.html());
-            notEqual($fixture.html(),'','view is rendered');
-            equal($fixture.find('.result-item').length,10,'result item is correct');
-            var $pagination = $fixture.find('.result-pagination');
-            notEqual($pagination.html(),'','result pagination is rendered');
-            notEqual($pagination.html().indexOf('上一页'),-1,'pagination has pre page');
-            notEqual($pagination.html().indexOf('下一页'),-1,'pagination has next page');
-            equal($pagination.find('a').length,12,'pagination rendered correctly');
-        });
-    };
-    return {run:run};
-});
+            test('result detail view-infu',function(){
+                var model = factory.create(Fixture.Infu);
+                var view = new ResultDetailView({
+                    el:'#qunit-fixture',
+                    model:model
+                });
+                view.render();
+                var $fixture = $('#qunit-fixture');
+                console.log($fixture.html());
+                notEqual($fixture.html(),'','result detail view is rendered');
+            });
+            test('result detail view-lib',function(){
+                var model = factory.create(Fixture.Lib);
+                var view = new ResultDetailView({el:'#qunit-fixture',model:model});
+                view.render();
+                var $fixture = $('#qunit-fixture');
+                notEqual($('.result-title',$fixture).html(),'');
+                notEqual($('.result-answer',$fixture).html(),'');
+            });
+            test('result detail view-chemschool',function(){
+                var model = factory.create(Fixture.Chemschool);
+                var view = new ResultDetailView({el:'#qunit-fixture',model:model});
+                view.render();
+                var $fixture = $(view.el);
+                notEqual($('.result-title',$fixture).html(),'');
+                notEqual($('.result-time',$fixture).html(),'');
+                notEqual($('.result-question',$fixture).html(),'');
+                notEqual($('.result-answer',$fixture).html(),'');
+            });
+            test('result detail view-myhome',function(){
+                var model = factory.create(Fixture.MyHome);
+                var view = new ResultDetailView({el:'#qunit-fixture',model:model});
+                view.render();
+                var $fixture = $(view.el);
+                notEqual($('.result-title',$fixture).html(),'');
+                notEqual($('.result-question',$fixture).html(),'');
+            });
+            test('result detail view-career',function(){
+                var model = factory.create(Fixture.MyHome);
+                var view = new ResultDetailView({el:'#qunit-fixture',model:model});
+                view.render();
+                var $fixture = $(view.el);
+                notEqual($('.result-title',$fixture).html(),'');
+                notEqual($('.result-question',$fixture).html(),'');
+                notEqual($('.result-answer',$fixture).html(),'');
+            });
+            test('result detail view-tunet',function(){
+                var model = factory.create(Fixture.Tunet);
+                var view = new ResultDetailView({el:'#qunit-fixture',model:model});
+                view.render();
+                var $fixture = $(view.el);
+                notEqual($('.result-title',$fixture).html(),'');
+                notEqual($('.result-time',$fixture).html(),'');
+                notEqual($('.result-answer',$fixture).html(),'');
+            });
+        };
+        return {run:run};
+        }
+);
