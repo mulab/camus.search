b58c4a6dbaa0e570f93a5f8358b18359f200d1c9
cgcgbcbc <cgcgbcbc@163.com>
2014-02-01 13:37:06 +0800

    add query model and tests

---
+++ infu/frontend/js/app/model/query.js
@@ -0,0 +1,17 @@
+/**
+ * Created by guangchen on 1/29/14.
+ */
+"use strict";
+define(['backbone'],function (Backbone) {
+    var query = Backbone.Model.extend({
+        defaults:{
+            keywords:'',
+            type:0,
+            start:0
+        },
+        serialize: function () {
+            return '#query/k'+this.get('keywords')+'/t'+this.get('type')+'/s'+this.get('start');
+        }
+    });
+    return query;
+});
