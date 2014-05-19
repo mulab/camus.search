76c38fc293026f5bc00cb630a741eef067c55a16
cgcgbcbc <cgcgbcbc@163.com>
2014-05-20 05:28:30 +0800

    add tests for model info

---
+++ infu/yo-frontend/test/fixtures/infu.js
@@ -0,0 +1,23 @@
+'use strict';
+define(function () {
+    var infu = {
+        'id': 0,
+        'author': '\u7ae0\u5c27\u65f8Martin',
+        'authorId': '320182196',
+        'commentCount': 1,
+        'commentList': [
+            {
+                'body': '\u653e\u8f7b\u677e \u4ed6\u51fa\u9898\u5f88\u6709\u65b0\u610f\u6211\u89c9\u5f97 \u6bcf\u4e2a\u9898\u76ee\u611f\u89c9\u90fd\u662f\u4e2a\u5c0f\u6545\u4e8b \u4e2d\u95f4\u7684\u9898\u8981\u662f\u5148\u60f3\u4e0d\u51fa\u6765\u5c31\u53bb\u770b\u540e\u9762\u7684 \u4ed6\u7684\u7406\u8bba\u662f \u597d\u591a\u4eba\u5149\u60f3\u5728\u4e2d\u95f4\u7684\u9898\u82b1\u529f\u592b \u5bfc\u81f4\u540e\u9762\u7684\u9898\u6ca1\u65f6\u95f4\u505a \u6240\u4ee5\u4ed6\u4f1a\u628a\u540e\u9762\u51e0\u9053\u9898\u51fa\u7684\u7b80\u5355\u70b9 \u6211\u4e0a\u6b21\u662f\u8fd9\u6837 \u4e0d\u77e5\u9053\u8fd9\u6b21\u8fd8\u662f\u4e0d\u662f\u4e86\n',
+                'name': '\u9093\u5174\u745e',
+                'time': '2013-05-06 23:56'
+            }
+        ],
+        'largeUrl': 'http://fmn.rrimg.com/fmn065/20130506/2310/p_large_8uRs_71600000141c1261.jpg',
+        'photoId': '7134803402',
+        'resultIndex': 6,
+        'time': '2013-05-06 23:10:21.0',
+        'title': ' \u660e\u5929\u8001\u6bd5\u7684\u5927\u96fe\u82f1\u8981\u8003\u8bd5\u4e86\uff0c\u600e\u4e48\u590d\u4e60\u554a\uff1f\u6709\u4ec0\u4e48\u9898\u53ef\u505a\u4e48\uff1f',
+        'type': 'infu'
+    };
+    return infu;
+});
