92ef65d51e8476cbe2b7436c723505b23a329409
cgcgbcbc <cgcgbcbc@163.com>
2014-05-20 08:09:30 +0800

    add detail view and test

--- infu/yo-frontend/test/spec/views.js
+++ infu/yo-frontend/test/spec/views.js
@@ -1,7 +1,7 @@
 /*jshint qunit: true*/
 /*global sinon: false*/
 'use strict';
-define(['views/resultList', '../fixtures/infu', 'collections/infu', 'jquery'], function (ResultList, Fixture, Infu, $) {
+define(['views/resultList', 'views/resultDetail', '../fixtures/infu', 'collections/infu', 'jquery'], function (ResultList, ResultDetail, Fixture, Infu, $) {
     return {
         run: function () {
             test('result list view', function () {
@@ -34,6 +34,33 @@ define(['views/resultList', '../fixtures/infu', 'collections/infu', 'jquery'], f
                 notEqual($pagination.html().indexOf('上一页'), -1, 'pagination has pre page');
                 notEqual($pagination.html().indexOf('下一页'), -1, 'pagination has next page');
                 equal($pagination.find('a').length, 12, 'pagination rendered correctly');
+            });
+            test('result detail view', function () {
+                var server = sinon.fakeServer.create();
+                server.respondWith('GET', '/search?type=infu&keywords=&start=10&size=10', [200, {
+                        'Content-Type': 'application/json'
+                },
+                    JSON.stringify({
+                        result: [Fixture],
+                        count: 100
+                    })]);
+                var infus = new Infu([], {
+                    keywords: '',
+                    start: 10,
+                    pageSize: 10
+                });
+                infus.fetch();
+                server.respond();
+                equal(infus.length, 1);
+                var view = new ResultDetail({
+                    el: '#qunit-fixture',
+                    model: infus.models[0]
+                });
+                view.render();
+                var $fixture = $('#qunit-fixture');
+                notEqual($fixture.html(), '', 'view is rendered');
+                equal($fixture.find('.result-title').html(), Fixture.title, 'title rendered');
+                equal($fixture.find('.result-large-url img').attr('src'), Fixture.largeUrl, 'picture rendered');
             });
         }
     };
