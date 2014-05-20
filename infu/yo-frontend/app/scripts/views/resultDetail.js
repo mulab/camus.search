8f5143b301beaade02b0cab53d44f27932863d1e
cgcgbcbc <cgcgbcbc@163.com>
2014-05-20 08:09:30 +0800

    add detail view and test

---
+++ infu/yo-frontend/app/scripts/views/resultDetail.js
@@ -0,0 +1,32 @@
+/*global define*/
+
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'templates'
+], function ($, _, Backbone, JST) {
+    'use strict';
+
+    var ResultDetailView = Backbone.View.extend({
+        template: JST['app/scripts/templates/resultDetail.hbs'],
+
+        tagName: 'div',
+
+        id: '',
+
+        className: '',
+
+        events: {},
+
+        initialize: function () {
+            this.listenTo(this.model, 'change', this.render);
+        },
+
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
+        }
+    });
+
+    return ResultDetailView;
+});
