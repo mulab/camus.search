6099014d137ef6005c2d928f8c718da9507c3a7e
cgcgbcbc <cgcgbcbc@163.com>
2014-05-21 23:06:39 +0800

    delete route /search in flask

--- infu/app/views.py
+++ infu/app/views.py
@@ -11,11 +11,6 @@ __es_url__ = '127.0.0.1:9200'
 def home():
     return render_template('home.html', app=app)
 
-@app.route('/search')
-def index():
-    return render_template('index.html', app=app)
-
-
 def get_query_params_from(req):
     if req.method == 'POST' and req.data != '':
         args = jsonlib.loads(req.data)
