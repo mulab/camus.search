32424c963d79d7c04be62b2ea31c641241a94d1b
cgcgbcbc <cgcgbcbc@163.com>
2014-05-22 01:13:44 +0800

    handle click function and render detail

--- infu/yo-frontend/app/scripts/views/result.js
+++ infu/yo-frontend/app/scripts/views/result.js
@@ -6,8 +6,9 @@ define([
     'backbone',
     'templates',
     'views/searchBox',
-    'views/resultList'
-], function ($, _, Backbone, JST, SearchBox, ResultList) {
+    'views/resultList',
+    'views/resultDetail'
+], function ($, _, Backbone, JST, SearchBox, ResultList, ResultDetail) {
     'use strict';
 
     var ResultView = Backbone.View.extend({
@@ -19,7 +20,9 @@ define([
 
         className: '',
 
-        events: {},
+        events: {
+            'click .result-item': 'showDetail'
+        },
 
         initialize: function () {
             this.listenTo(this.model, 'change', this.render);
@@ -37,6 +40,19 @@ define([
             this.$el.html(this.template(this.model.toJSON()));
             this.searchBox.setElement('#searchBox').render();
             this.resultList.setElement('.result-list').render();
+        },
+
+        showDetail: function (event) {
+            var $target = $(event.currentTarget),
+                index = $target.attr('result-index'),
+                model = this.model.models[index],
+                view = new ResultDetail({
+                    el: '.result-detail',
+                    model: model
+                });
+            this.$el.find('.result-selected').removeClass('result-selected');
+            $target.addClass('result-selected');
+            view.render();
         }
     });
 
